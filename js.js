ef2cc375c2329774fd956cafb9b00e2bc48d113f
Sakine Mazlomyar <mazlomyar12@gmail.com>
2018-10-01 20:05:19 +0200

    add cssSlideShow

--- js.js
+++ js.js
@@ -1,18 +1,29 @@
-
-
+/* here is a functin for changing swedish sek to english dollar */
 function SwedishUsd(){
     var swedishInputMoney = document.getElementById("swedish").value;
-    var englishInputMoney = document.getElementById("usd").value;
-    console.log(swedishInputMoney, englishInputMoney)
+    var valueEnglishUsdMoneyOfOneDollar = 0.11;
+
     var x=3;
     var y =5;
     if ( x<y){
-        var totalMoney= swedishInputMoney * englishInputMoney;
-        alert(totalMoney)
-        
-
+        var totalMoneyToUsd= swedishInputMoney * valueEnglishUsdMoneyOfOneDollar;
+        var englishInputMoney = document.getElementById("usd").value = totalMoneyToUsd;
     }
 }
+/* here is a function for changing usd to swedish sek */
+function Usdswedish(){
+    var englishInputMoney = document.getElementById("usdTwo").value;
+    var valueSwedishMoneyOfOneSek = 8.95;
+    var x=3;
+    var y =5;
+    if ( x<y){
+        var totalMoneyToSwedish=  englishInputMoney * valueSwedishMoneyOfOneSek;
+        var swedishInputMoney = document.getElementById("swedishSekTwo").value = totalMoneyToSwedish ;
+    }
+}
+
+/* here is a function for our slideshow */
+
 
 function myPictureSlide(){
     var myPictureInput = document.getElementById("content");
